77c706f5bf80292e5d8df035aa110b77e3ad223f
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-11-11 00:32:56 +0900

    feat: 프로모션 기간인지 확인하는 기능 추가

--- src/Model/ProductPurchase.js
+++ src/Model/ProductPurchase.js
@@ -1,3 +1,5 @@
+import getCurrentDate from "../Utils/getCurrentDate";
+
 class ProductPurchase {
   constructor(products, purchaseItems) {
     this.products = products;
@@ -25,6 +27,19 @@ class ProductPurchase {
 
       return product; // [{프로모션 상품 객체}, {일반 상품 객체}]
     });
+  }
+
+  #checkIsPromotionTerm() {
+    const currentDate = getCurrentDate();
+    const product = this.#findProductByName();
+    const startDate = Number(product[0].promotion?.start_date.replace(/-/g, ''));
+    const endDate = Number(product[0].promotion?.end_date.replace(/-/g, ''));
+
+    if (!product[0].promotion) {
+      return;
+    }
+
+    return currentDate >= startDate && currentDate <= endDate;
   }
 }
 
