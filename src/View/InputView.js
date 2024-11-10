599dc2c7e5df70c434fe6c5bf114b810682fdff0
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-11-11 04:28:56 +0900

    chore: 프로모션 재고 부족 시 출력할 안내 메시지 추가

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,8 +1,10 @@
-import IOUtils from "../utils/IOUtils.js";
+import IOUtils from '../utils/IOUtils.js';
 
 class InputView {
   #inputMsg = Object.freeze({
-    PURCHASE: '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n'
+    PURCHASE: '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n',
+    NO_PROMOTION_WARNING: (name, quantity) =>
+      `현재 ${name} ${quantity}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n`,
   });
 
   async inputPurchaseItems() {
@@ -10,6 +12,13 @@ class InputView {
     IOUtils.newLine();
 
     return purchaseItems;
+  }
+
+  async inputNoPromotionWarning(name, quantity) {
+    const isPurchase = await IOUtils.input(this.#inputMsg.NO_PROMOTION_WARNING(name, quantity));
+    IOUtils.newLine();
+
+    return isPurchase;
   }
 }
 
