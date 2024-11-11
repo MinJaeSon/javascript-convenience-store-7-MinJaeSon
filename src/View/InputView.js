5d197cb8de8ab88c28f5fea41da56507dfebf038
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-11-11 18:00:53 +0900

    chore: 추가 상품 증정 가능성에 대한 사용자 입력 추가

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -5,6 +5,8 @@ class InputView {
     PURCHASE: '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n',
     NO_PROMOTION_WARNING: (name, quantity) =>
       `현재 ${name} ${quantity}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n`,
+    NOTIFY_PROMOTION_BENEFIT: (name, quantity) =>
+      `현재 ${name}은(는) ${quantity}개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n`,
   });
 
   async inputPurchaseItems() {
@@ -19,6 +21,13 @@ class InputView {
     IOUtils.newLine();
 
     return isPurchase;
+  }
+
+  async inputNotifyPromotionBenefit(name, quantity) {
+    const isAdd = await IOUtils.input(this.#inputMsg.NOTIFY_PROMOTION_BENEFIT(name, quantity));
+    IOUtils.newLine();
+
+    return isAdd;
   }
 }
 
