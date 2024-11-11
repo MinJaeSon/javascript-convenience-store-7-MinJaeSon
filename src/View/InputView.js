06ddb16fadc0d039f1acdc4a9baea96334021323
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-11-11 22:40:51 +0900

    chore: 멤버십 할인 적용 질문에 대한 입력 처리 추가

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -7,6 +7,7 @@ class InputView {
       `현재 ${name} ${quantity}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n`,
     NOTIFY_PROMOTION_BENEFIT: (name, quantity) =>
       `현재 ${name}은(는) ${quantity}개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n`,
+    MEMBERSHIP_APPLY: '멤버십 할인을 받으시겠습니까? (Y/N)',
   });
 
   async inputPurchaseItems() {
@@ -17,17 +18,28 @@ class InputView {
   }
 
   async inputNoPromotionWarning(name, quantity) {
-    const willPurchaseGeneral = await IOUtils.input(this.#inputMsg.NO_PROMOTION_WARNING(name, quantity));
+    const willPurchaseGeneral = await IOUtils.input(
+      this.#inputMsg.NO_PROMOTION_WARNING(name, quantity),
+    );
     IOUtils.newLine();
 
     return willPurchaseGeneral;
   }
 
   async inputNotifyPromotionBenefit(name, quantity) {
-    const willGetPresent = await IOUtils.input(this.#inputMsg.NOTIFY_PROMOTION_BENEFIT(name, quantity));
+    const willGetPresent = await IOUtils.input(
+      this.#inputMsg.NOTIFY_PROMOTION_BENEFIT(name, quantity),
+    );
     IOUtils.newLine();
 
     return willGetPresent;
+  }
+
+  async inputMembershipApply() {
+    const willApplyMembership = await IOUtils.input(this.#inputMsg.MEMBERSHIP_APPLY);
+    IOUtils.newLine();
+
+    return willApplyMembership;
   }
 }
 
